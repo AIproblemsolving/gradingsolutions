ee0724c116846b808146c81baf5a78a8f957a6fd
AIproblemsolving <automation.bot.as@gmail.com>
2024-10-27 18:48:38 +0100

    Update layout.py

--- layout.py
+++ layout.py
@@ -42,17 +42,3 @@ def emptylines():
     st.write("")
     st.write("")
 
-def hide_st():
-    hide_st = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        #stDecoration {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-        ._profileContainer_1yi6l_53 {display: none;}
-        ._profilePreview_1yi6l_63 {display: none;}
-        div[class*="_profileContainer_1yi6l_53"] {display: none;}
-        div[class*="_profilePreview_1yi6l_63"] {display: none;}
-        </style>
-        """
-    st.markdown(hide_st, unsafe_allow_html = True)
