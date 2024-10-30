68e6ba523780534aa8ed27932ce8d43c9c6ec482
AIproblemsolving <automation.bot.as@gmail.com>
2024-10-30 21:35:32 +0100

    Update security.py

--- security.py
+++ security.py
@@ -4,13 +4,13 @@ hashed_password = "b8ea7b7963a0fa7baaf4d71f4f0dc75a42aa7e4e1f3a406c809ca5b16ac7e
 hashed_api_keys = [
     "e08238f0174d5c63c56780b1e0587fcb123f4d3441a9008c973e9177c1dffd97",
     "0d4ca4d734049d4c5f0cc6cb5928492a3ad85068de45e0cdbadefbe1af37545e",
-    "placeholder"
+    "b27e587b8a57fb8bbb8c57e51d229884366a14e29e54d64974871b4c13c4d810"
 ]
 
 api_key_user = [
     "admin",
     "beta.user",
-    "placeholder"
+    "NewAPI.Test"
 ]
 
 def hash_input(input_text):
